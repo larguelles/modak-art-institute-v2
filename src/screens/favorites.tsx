933e0fb4f747fc65aee113ea0bcb777f42710558
Luis Arguelles <luismaria@androbin.co>
2024-01-23 19:30:44 -0300

    Build details favorites screen.

--- src/screens/favorites.tsx
+++ src/screens/favorites.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, FC} from 'react';
+import {View, FlatList} from 'react-native';
+import Card from '../components/card';
+import Header from '../components/header';
+import useArtworkStore, {Artwork} from '../api/store';
 
-const Favorites = () => {
+const viewStyle = {flex: 1};
+
+const Favorites: FC = () => {
+  const {artworks, fetchFavorites} = useArtworkStore();
+
+  useEffect(() => {
+    fetchFavorites();
+  }, [fetchFavorites]);
+
+  const renderItem = ({item}: {item: Artwork}) => {
+    return <Card item={item} iiifUrl={artworks?.config?.iiif_url} />;
+  };
+
   return (
-    <View>
-      <Text>Favorites</Text>
+    <View style={viewStyle}>
+      <Header title="Favorites" renderGoBack />
+      <FlatList data={artworks?.data} renderItem={renderItem} />
     </View>
   );
 };
