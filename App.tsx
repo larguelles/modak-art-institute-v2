bc017d1fb5fcab377fe6b529ec029e8f142b220e
Luis Arguelles <luismaria@androbin.co>
2024-01-23 19:24:18 -0300

    Add support for icons, also type the stack navigator.

--- App.tsx
+++ App.tsx
@@ -6,14 +6,23 @@ import Details from './src/screens/details';
 import Favorites from './src/screens/favorites';
 import SubmitArtwork from './src/screens/submit-artwork';
 import {Artwork} from './src/api/store';
-
-const Stack = createNativeStackNavigator();
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {
+  faHome as home,
+  faHeart as heart,
+  faArrowLeft as arrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import {faHeart as heartOutline} from '@fortawesome/free-regular-svg-icons';
+library.add(home, heart, heartOutline, arrowLeft);
 
 export type RootStackParamList = {
   Home: undefined;
   Details: {data: Artwork; uri: string};
   Favorites: undefined;
+  SubmitArtwork: undefined;
 };
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = (): React.JSX.Element => {
   const screenOptions = {headerShown: false};
