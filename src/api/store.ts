1f2ca4dc28c228f406a109edacdb24af783dfbf0
Luis Arguelles <luismaria@androbin.co>
2024-01-23 19:27:49 -0300

    Limit fetched artwork fields to only those required, add pagination feature, pass proper config and info data when fetching by id, add feature to remove from favorites and fix fetching of favorites.

--- src/api/store.ts
+++ src/api/store.ts
@@ -28,59 +28,21 @@ export type ArtworksResponse = {
 
 export type Artwork = {
   id: number;
-  api_model: string;
-  api_link: string;
-  is_boosted: boolean;
+  image_id: string;
   title: string;
-  alt_titles: null | string[];
   thumbnail: {
     lqip: string;
     width: number;
     height: number;
     alt_text: string;
   };
-  main_reference_number: string;
-  has_not_been_viewed_much: boolean;
-  boost_rank: number;
-  date_start: number;
-  date_end: number;
   date_display: string;
-  date_qualifier_title: string;
-  date_qualifier_id: null | number;
   artist_display: string;
   place_of_origin: string;
   description: string;
-  short_description: string;
   dimensions: string;
-  dimensions_detail: [
-    {
-      depth: null;
-      width: number;
-      height: number;
-      diameter: null;
-      clarification: null;
-    },
-  ];
   medium_display: string;
-  inscriptions: null;
   credit_line: string;
-  catalogue_display: null;
-  publication_history: null;
-  exhibition_history: null;
-  provenance_text: null;
-  edition: null;
-  publishing_verification_level: string;
-  internal_department_id: number;
-  fiscal_year: number;
-  fiscal_year_deaccession: null;
-  is_public_domain: boolean;
-  is_zoomable: boolean;
-  max_zoom_window_size: number;
-  copyright_notice: null;
-  has_multimedia_resources: boolean;
-  has_educational_resources: boolean;
-  has_advanced_imaging: boolean;
-  colorfulness: number;
   color: {
     h: number;
     l: number;
@@ -88,92 +50,70 @@ export type Artwork = {
     percentage: number;
     population: number;
   };
-  latitude: number;
-  longitude: number;
-  latlon: string;
-  is_on_view: boolean;
-  on_loan_display: null;
-  gallery_title: string;
-  gallery_id: number;
-  nomisma_id: null;
-  artwork_type_title: string;
-  artwork_type_id: number;
-  department_title: string;
-  department_id: string;
-  artist_id: number;
-  artist_title: string;
-  alt_artist_ids: number[];
-  artist_ids: number[];
-  artist_titles: string[];
-  category_ids: string[];
   category_titles: string[];
-  term_titles: string[];
-  style_id: string;
-  style_title: string;
-  alt_style_ids: string[];
-  style_ids: string[];
-  style_titles: string[];
-  classification_id: string;
-  classification_title: string;
-  alt_classification_ids: string[];
-  classification_ids: string[];
-  classification_titles: string[];
-  subject_id: string;
-  alt_subject_ids: string[];
-  subject_ids: string[];
-  subject_titles: string[];
-  material_id: string;
-  alt_material_ids: string[];
-  material_ids: string[];
-  material_titles: string[];
-  technique_id: string;
-  alt_technique_ids: string[];
-  technique_ids: string[];
-  technique_titles: string[];
-  theme_titles: string[];
-  image_id: string;
-  alt_image_ids: string[];
-  document_ids: string[];
-  sound_ids: string[];
-  video_ids: string[];
-  text_ids: string[];
-  section_ids: string[];
-  section_titles: string[];
-  site_ids: string[];
-  suggest_autocomplete_boosted: string;
-  suggest_autocomplete_all: Array<{
-    input: string[];
-    contexts: {
-      groupings: string[];
-    };
-    weight?: number;
-  }>;
-  source_updated_at: string;
-  updated_at: string;
-  timestamp: string;
 } & ResponseConfigAndInfo;
 
 type StoreType = {
-  artworks: ArtworksResponse | [];
-  artworkById: Artwork | {};
-  favorites: string[] | [];
-  fetchArtworks: () => Promise<void>;
+  artworks: ArtworksResponse;
+  artworkById: {[key: string]: Artwork};
+  favorites: string[];
+  fetchArtworks: (page?: number) => Promise<void>;
   fetchArtworkById: (id: string) => Promise<void>;
   fetchFavorites: () => Promise<void>;
   addToFavorites: (artworkId: string) => Promise<void>;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
 };
 
 const useArtworkStore = create<StoreType>(
   (set: StoreApi<StoreType>['setState']) => ({
-    artworks: [],
+    artworks: {
+      pagination: {
+        total: 0,
+        limit: 0,
+        offset: 0,
+        total_pages: 0,
+        current_page: 0,
+      },
+      data: [],
+      info: {
+        license_text: '',
+        license_links: [],
+        version: '',
+      },
+      config: {
+        iiif_url: '',
+        website_url: '',
+      },
+    },
+    currentPage: 1,
     artworkById: {},
     favorites: [],
-    fetchArtworks: async () => {
+    fetchArtworks: async (page = 1) => {
       try {
         const response: AxiosResponse<ArtworksResponse> = await axios.get(
-          'https://api.artic.edu/api/v1/artworks',
+          `https://api.artic.edu/api/v1/artworks?page=${page}&fields=id,image_id,title,thumbnail,artist_display,date_display,main_reference_number,credit_line,medium_display,place_of_origin,description,dimensions,color,category_titles`,
         );
-        set({artworks: response.data, artworkById: {}});
+
+        set(state => ({
+          artworks: {
+            pagination: response.data.pagination,
+            data:
+              page === 1
+                ? response.data.data
+                : [...state.artworks.data, ...response.data.data],
+            info: response.data.info,
+            config: response.data.config,
+          },
+          currentPage: page,
+          artworkById: {
+            ...state.artworkById,
+            ...response.data.data.reduce((acc, artwork) => {
+              acc[artwork.id] = artwork;
+              return acc;
+            }, {} as {[key: string]: Artwork}),
+          },
+        }));
       } catch (error) {
         console.error('Error fetching artworks:', error);
       }
@@ -181,30 +121,20 @@ const useArtworkStore = create<StoreType>(
     fetchArtworkById: async (id: string) => {
       try {
         const response: AxiosResponse<ArtworksResponse> = await axios.get(
-          `https://api.artic.edu/api/v1/artworks/${id}`,
+          `https://api.artic.edu/api/v1/artworks/${id}?fields=id,image_id,title,thumbnail,artist_display,date_display,main_reference_number,credit_line,medium_display,place_of_origin,description,dimensions,color,category_titles`,
         );
-        set({
+        set(state => ({
           artworks: {
-            pagination: {
-              total: 1,
-              limit: 1,
-              offset: 0,
-              total_pages: 1,
-              current_page: 1,
-            },
+            pagination: response.data.pagination,
             data: response.data.data,
-            info: {
-              license_text: '',
-              license_links: [],
-              version: '',
-            },
-            config: {
-              iiif_url: '',
-              website_url: '',
-            },
+            info: response.data.info,
+            config: response.data.config,
           },
-          artworkById: {[id]: response.data.data},
-        });
+          artworkById: {
+            ...state.artworkById,
+            [id]: response.data.data[0],
+          },
+        }));
       } catch (error) {
         console.error(`Error fetching artwork with ID ${id}:`, error);
       }
@@ -225,6 +155,25 @@ const useArtworkStore = create<StoreType>(
         console.error('Error adding to favorites:', error);
       }
     },
+    removeFromFavorites: async (artworkId: string) => {
+      try {
+        const currentFavoritesJSON = await AsyncStorage.getItem('favorites');
+        const currentFavorites: string[] = currentFavoritesJSON
+          ? JSON.parse(currentFavoritesJSON)
+          : [];
+        const indexToRemove = currentFavorites.indexOf(artworkId);
+        if (indexToRemove !== -1) {
+          currentFavorites.splice(indexToRemove, 1);
+          await AsyncStorage.setItem(
+            'favorites',
+            JSON.stringify(currentFavorites),
+          );
+          set({favorites: currentFavorites});
+        }
+      } catch (error) {
+        console.error('Error removing from favorites:', error);
+      }
+    },
     fetchFavorites: async () => {
       try {
         const favoriteIdsJSON = await AsyncStorage.getItem('favorites');
@@ -235,12 +184,12 @@ const useArtworkStore = create<StoreType>(
         const favoriteArtworks = await Promise.all(
           favoriteIds.map(async (id: string) => {
             const response = await axios.get(
-              `https://api.artic.edu/api/v1/artworks/${id}`,
+              `https://api.artic.edu/api/v1/artworks/${id}?fields=id,image_id,title,thumbnail,artist_display,date_display,main_reference_number,credit_line,medium_display,place_of_origin,description,dimensions,color,category_titles`,
             );
             return response.data.data;
           }),
         );
-        set({
+        set(state => ({
           artworks: {
             pagination: {
               total: favoriteArtworks.length,
@@ -250,21 +199,24 @@ const useArtworkStore = create<StoreType>(
               current_page: 1,
             },
             data: favoriteArtworks,
-            info: {
-              license_text: '',
-              license_links: [],
-              version: '',
-            },
+            info: {...state.artworks.info},
             config: {
-              iiif_url: '',
-              website_url: '',
+              ...state.artworks.config,
             },
           },
-          artworkById: {},
-        });
+          artworkById: favoriteArtworks.reduce((acc, artwork) => {
+            acc[artwork.id] = artwork;
+            return acc;
+          }, {} as {[key: string]: Artwork}),
+        }));
       } catch (error) {
         console.error('Error fetching favorite artworks:', error);
       }
+    },
+    setCurrentPage: (page: number) => {
+      set({
+        currentPage: page,
+      });
     },
   }),
 );
